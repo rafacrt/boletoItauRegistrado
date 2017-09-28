b0305f9ac6572104203aad9324514743175efb6f
Lucas Moura <lucasmoura.dev@gmail.com>
2017-09-28 17:07:41 -0300

    Update TokenItau.php

--- TokenItau.php
+++ TokenItau.php
@@ -7,23 +7,17 @@ use GuzzleHttp\Client;
 
 class TokenItau
 {
-    private $token = '7be0dc92270ca70966c31c291a87bf837be0dc92270ca70966c31c291a87bf83';
-
+ 
     public function __construct()
     {
         // Ao chamar Precisamo Gerar um Boleto;
     }
 
-    public function getToken()
-    {
-        return $this->token;
-    }
-
     public function gerartoken()
     {
          $dadosPedirToken = [
-             'client_id' => 'gNMh2GwifEOx0',
-             'client_secret' => 'R5W3M396i9NtHMCDC6fUsKll5Z4f1Yuf5SiyJvIthbjhvDi2t9v6kxwt3avlhvDAJZZMe3GYSeTJEUUkV4kRUg2',
+             'client_id' => 'NUMERO DO MEU CLIENTE ID',
+             'client_secret' => 'DADOS DO MEU CLIENT SECRET',
              'grant_type' => 'client_credentials',
              'scope' => 'readonly'];
 
